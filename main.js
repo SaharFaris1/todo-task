206ed4e04e551947cdb053ba89c63317492bee1b
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-05-13 00:09:46 +0300

    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let inputBox = document.getElementById("input-box");
 let listContainer = document.getElementById("list-container");
+let clearDataBtn = document.getElementById("clearDataBtn");
 
 function addTask() {
     if (inputBox.value === '') {
@@ -7,23 +8,43 @@ function addTask() {
     } else {
         let li = document.createElement("li");
         li.innerText = inputBox.value;
-        listContainer.appendChild(li);
 
         let span = document.createElement("span");
-        span.innerText = "\u00d7"; 
+        span.innerText = "\u00d7"; // رمز الحذف
         li.appendChild(span);
 
+        let editBtn = document.createElement("button");
+        editBtn.innerText = "Edit"; 
+        editBtn.className = "edit-btn"; 
+
+
+
+        editBtn.onclick = function() {
+            let newTask = prompt("Edit your task:", li.innerText);
+            if (newTask !== null && newTask !== '') {
+                li.firstChild.nodeValue = newTask; 
+                saveData(); 
+            }
+        };
+        li.appendChild(editBtn); 
+
+        listContainer.appendChild(li);
         inputBox.value = ""; 
-        saveData(); 
+        saveData();
+        showClearButton(); 
     }
 }
+
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
     } else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove(); 
-        saveData();
+        if (confirm("Are you sure you want to delete this task?")) {
+            e.target.parentElement.remove(); 
+            saveData(); 
+            showClearButton(); 
+        }
     }
 }, false);
 
@@ -33,13 +54,18 @@ function saveData() {
 
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data") || ''; 
+    showClearButton(); 
 }
 
-let clearDataBtn = document.getElementById("clearDataBtn");
+function showClearButton() {
+    clearDataBtn.style.display = listContainer.innerHTML ? "block" : "none";
+}
+
 clearDataBtn.addEventListener("click", function() {
     if (confirm("Are you sure you want to delete all tasks?")) {
         localStorage.removeItem("data"); 
         listContainer.innerHTML = ""; 
+        showClearButton(); 
     }
 });
 
